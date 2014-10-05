a2b9ee8033c582f88c9d418fb93af7486fc47a10
Daniel Lamando <dan@danopia.net>
2014-10-05 05:18:51 -0700

    Push displays out to logged-in non-displays

--- control/control.js
+++ control/control.js
@@ -9,4 +9,17 @@ if (Meteor.isClient) {
       });
     },
   });
+
+  Template.Control.rendered = function () {
+    Meteor.subscribe('displays');
+  };
+
+} else if (Meteor.isServer) {
+  Meteor.publish('displays', function () {
+    if (this.userId && Meteor.users.findOne(this.userId).profile.type != 'display')
+      return Meteor.users.find({'profile.type': 'display'}, {fields: {profile: 1}});
+    else
+      return [];
+  });
+
 }
