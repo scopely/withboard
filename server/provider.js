0321a20383ccccc4a048244f52cf43351968a54a
Daniel Lamando <daniel.lamando@scopely.com>
2014-11-03 09:44:59 -0800

    Add provider methods

---
+++ server/provider.js
@@ -0,0 +1,24 @@
+Meteor.methods({
+  setupProvider: function () {
+    console.log('Provider connected');
+    Config.upsert({key: 'has-provider'}, {key: 'has-provider', value: true});
+    State.upsert({key: 'has-provider'}, {key: 'has-provider', value: true});
+    return 'cool';
+  },
+
+  setConfig: function (key, value) {
+    console.log('Provider set config', key, 'to', value);
+    return Config.upsert({key: key}, {key: key, value: value, source: 'provider'});
+  },
+  setState: function (key, value) {
+    console.log('Provider set state', key, 'to', value);
+    return State.upsert({key: key}, {key: key, value: value, source: 'provider'});
+  },
+});
+
+Meteor.publish('config', function () {
+  return Config.find();
+});
+Meteor.publish('state', function () {
+  return State.find();
+});
